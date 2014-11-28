c5d6adc92a19d33eb5b0344962c39320d04b1800
pilo <jakub.pilimon@4finance.com>
2014-11-28 13:39:31 +0100

    initial bank statement controller

---
+++ src/main/groovy/com/ofg/bankparsers/BankParserController.groovy
@@ -0,0 +1,13 @@
+package com.ofg.bankparsers
+
+import groovy.util.logging.Slf4j
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Slf4j
+@RestController
+@RequestMapping('/api')
+class BankParserController {
+
+
+}
