3f007d4787a1fc355ec29ea191b36570d61d5863
pilo <jakub.pilimon@4finance.com>
2014-11-28 14:23:19 +0100

    BankStatementParser applicapbleFor predicate done in java8 style

--- src/main/groovy/com/ofg/bankstatement/parser/BankStatementParser.java
+++ src/main/groovy/com/ofg/bankstatement/parser/BankStatementParser.java
@@ -3,29 +3,18 @@ package com.ofg.bankstatement.parser;
 import com.google.common.base.Predicate;
 import com.ofg.loans.api.beans.payment.PaymentInfo;
 
-import javax.annotation.CheckForNull;
-import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
 import java.io.InputStream;
 import java.util.List;
 
 public interface BankStatementParser {
 
-    List<PaymentInfo> importBankStatement(@CheckForNull InputStream is);
+    List<PaymentInfo> importBankStatement(@NotNull InputStream is);
 
     boolean isApplicableFor(String fileName);
 
-    public class ApplicableForPredicate implements Predicate<BankStatementParser> {
-
-        private final String fileName;
-
-        public ApplicableForPredicate(String fileName) {
-            this.fileName = fileName;
-        }
-
-        @Override
-        public boolean apply(@Nullable BankStatementParser bankStatementParser) {
-            return bankStatementParser.isApplicableFor(fileName);
-        }
-
+    public static Predicate<BankStatementParser> applicableFor(String fileName) {
+        return p -> p.isApplicableFor(fileName);
     }
+
 }
